28a170bca78cfd91fcc38b907aecfca79a692dfc
hyunu0504 <domaup@naver.com>
2023-07-04 21:32:41 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -6,11 +6,47 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
+    
 );
 
+// class 문법 consturctor 생략 버전
+
+class humanTall{
+        tall = "173CM"
+    printTall = () => {
+        console.log(this.tall)
+    }
+}
+
+
+class human extends humanTall{
+    name = "hong"
+    height = "66kg"
+
+    printName(){
+        console.log(this.name)
+    }
+    printHeight(){
+        console.log(this.height)
+    }
+}
+
+
+const myInfo = new human;
+
+myInfo.printName();
+myInfo.printHeight();
+myInfo.printTall();
+
+// 구조분해
+
+const numbers = [1,2,3];
+const [num1, , num3] = numbers;
+console.log(num1, num3)
+ // = 1, 3 출력
+
+  
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
